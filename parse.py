1b3250e66486bfa09199e4007d78d858164f1b2c
zjorsie <zjorsie@users.noreply.github.com>
2018-02-26 12:16:50 +0100

    update, list not neccessary
    update, list not neccessary

--- parse.py
+++ parse.py
@@ -5,31 +5,29 @@ file = 'output.json'
 json_data = open(file).read()
 data = json.loads(json_data)
 
-maclist = list()
-time = dict()
+d_mac = dict()
 for x in data:
         for mac in x['_source']['layers']['eth.addr']:
                 if mac != 'ff:ff:ff:ff:ff:ff':
-                        if mac not in maclist:
-                                maclist.append(mac)
-                                time[mac] = list()
-                        time[mac].append(x['_source']['layers']['frame.time_relative'][0])
+                        if mac not in d_mac:
+                                d_mac[mac] = list()
+                        d_mac[mac].append(x['_source']['layers']['frame.time_relative'][0])
 
 # we now have a dictionary with lists of the times of the packets:
 # first arrival of mac's packet:
-for x in time:
+for x in d_mac:
         print 'First entry of MAC '+x+'. Time: '+time[x][0]
 
 # Last arrival of packet
-for x in time:
+for x in d_mac:
         print 'Last entry of MAC '+x+'. Time: '+time[x][-1]
 
 # Time spent:
-for x in time:
+for x in d_mac:
         print 'Time spent from MAC: '+x+'. Time on network: '+str(float(time[x][-1]) - float(time[x][0]))
 
 # find out vendor:
-fox x in time:
+fox x in d_mac:
         print 'MAC: '+x+'. Vendor:'
 
 # We can build very fancy graphs here.
