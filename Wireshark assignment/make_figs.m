83544c7b09a0b1597305bc7d5793d9a16fb900b7
Sjors Nijhuis <sjorsnijhuis@gmail.com>
2018-03-06 20:47:27 +0100

    fix axis type size

--- Wireshark assignment/make_figs.m
+++ Wireshark assignment/make_figs.m
@@ -6,20 +6,21 @@ T3 = readtable('numclients.csv');
 A1 = table2array(T1);
 A2 = table2array(T2);
 A3 = table2array(T3);
-
+fontsize_title = 18;
+fontsize_axis = 14;
 figure
 set(gcf,'units','points','position',[0,0,900,600])
 hold on
 yyaxis left;
-ylabel('# of (dis)connects')
+ylabel('# of (dis)connects','fontsize',fontsize_axis)
 stem(A1(:,1),A1(:,2), 'filled', 'green');
 stem(A2(:,1),A2(:,2), 'filled');
 yyaxis right;
 plot(A3(:,1),A3(:,2))
 hold off
-xlabel('time (s)')
-ylabel('# of connected clients')
-title('The number of (dis)connects of the network per second and total number of clients')
+xlabel('time (s)','fontsize',fontsize_axis)
+ylabel('# of connected clients','fontsize',fontsize_axis)
+title('The number of (dis)connects of the network per second and total number of clients','fontsize',fontsize_title )
 xlim([min(min(A1(:,1)),min(A2(:,1))) max(max(A1(:,1)),max(A2(:,1)))])
 legend([T1.Properties.VariableNames(2),T2.Properties.VariableNames(2), 'Number of connected clients'])
 
@@ -40,15 +41,15 @@ name = cellstr(labels);
 
 bar(vals)
 set(gca,'xticklabel',name)
-ylabel('# of distinctive addresses')
-title('Number of MAC addresses from vendor')
+ylabel('# of distinctive addresses','fontsize',fontsize_axis)
+title('Number of MAC addresses from vendor','fontsize',fontsize_title )
 print('vendor','-dpng')
 
 figure
 set(gcf,'units','points','position',[0,0,900,600])
 bar(n_time)
-title('Average time on network per vendor')
-ylabel('time(s)')
+title('Average time on network per vendor','fontsize',fontsize_title)
+ylabel('time(s)','fontsize',fontsize_axis)
 set(gca,'xticklabel',name)
 print('timeonnetwork','-dpng')
 
