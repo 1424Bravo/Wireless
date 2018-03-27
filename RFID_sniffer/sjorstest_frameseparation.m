631604638bb944904783587ff11aa2356c243e2e
Sjors Nijhuis <sjorsnijhuis@gmail.com>
2018-03-27 11:37:23 +0200

    update, werkt beter

--- RFID_sniffer/sjorstest_frameseparation.m
+++ RFID_sniffer/sjorstest_frameseparation.m
@@ -8,23 +8,26 @@ clc;
 % 'finished' in a transmission way. this value is stored in the variable
 % 'array_end'. The signal can be read with intervals of the sample numbers
 % of the frame_end array, to read one single frame per time. 
-
-
-ARRAY = PUT_HERE_THE_SIGNAL_VARIABLE;
+n = 1;
+t = linspace(0,20*pi);
+x = square(t);
+plot(t,x);
+ARRAY = x;
 threshold = 0.6;    % Threshold signal level that indicates start of a frame: probably implement something with moving averages here, or just do it after normalizing.
-samplerate = 100;   % samples per second
+samplerate = 1;   % samples per second
 tlow = 0.5;         % Time before a packet has 'ended'. t in seconds
+frame_end = 0;
 
 
 %%%% NO VARIABLES SHOULD BE MODIFIED BELOW HERE!!! %%%%%
-while n < (length(ARRAY))
+while n < length(ARRAY)
     hight = 0;          % checks if the frame has started
     lowc = 0;           % number of samples the signal has been below the threshold
     eop = 0;            % end of packet has been found, write signal to array
-    p = 0;              % count the p't packet end.
+    p = 1;              % count the p't packet end.
 	% loop through array, if signal is high a packet has started. when the signal is then low for a 'long' 
 	% amount of time, the packet has ended.
-    while eop == 0
+    while eop == 0 && n<length(ARRAY)
 		% if signal is high: packet has started.
         
 		if ARRAY(n) >= threshold
@@ -37,7 +40,7 @@ while n < (length(ARRAY))
 		if (hight == 1) && (lowc > (samplerate * tlow)) 
             % end of frame detected: signal has been high in the past and
             % has been low for a while.
-			frame_end(p) = n; % dirty.
+			frame_end = [frame_end n]; % dirty.
             p = p + 1;
 			eop = 1;
             
