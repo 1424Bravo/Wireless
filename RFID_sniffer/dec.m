cad83d62771094912c18104b8f076cff26846cce
1424Bravo <k.p.vandermark@student.tudelft.nl>
2018-03-28 09:32:00 +0200

    fixed timing issues and added frame checks

--- RFID_sniffer/dec.m
+++ RFID_sniffer/dec.m
@@ -1,25 +1,15 @@
 function [bit,check] = dec(frame)
-    part = frame;
-    k_old=-1;
+    part = frame; k_old=-1;
     data = diff(part);
-    thresp=0.5;
-    thresm=-0.5;
-    rising = find(data>thresp)+1;
+    thresp=0.5; thresm=-0.5;
+    rising = find(data>thresp)+1; %find edges
     falling = find(data<thresm);
     high = falling(1:numel(rising)) - rising;
-   
-    for i=1:(length(rising)-1)
-        low(i) = rising(i+1) - falling(i);
-    end
-    low = low';
-    
-    period = ceil((min(abs(low))+min(abs(high)))/2);    
-
- nump = numel(falling)+numel(rising)-1;
- start = falling(1);
- 
- 
- falris = [falling;rising];
+    high(1)=99999; % remove initialization error
+    period = 2*min(abs(high));
+    nump = numel(falling)+numel(rising)-1;
+    start = falling(1);
+    falris = [falling;rising];
  
  for i=1:nump
      if mean(part(start:start+period))<0.25 || mean(part(start:start+period)) > 0.75
@@ -37,8 +27,6 @@ function [bit,check] = dec(frame)
      end
      k_old=k;
  end
- 
-
  pre = rot90(de2bi(hex2dec('BBBB')))';
  check = isequal(pre,bit(1:length(pre)));
 end
--- RFID_sniffer/wisent.m
+++ RFID_sniffer/wisent.m
@@ -15,9 +15,19 @@ thres = 0.045;%0.95*max(orig_sig);
 data = zeros(length(frame(1,:)),114);
 for i=3:length(frame(1,:))
    [bit,check]=dec(frame(:,i));
+   length(bit)
    data(i,1:length(bit))=bit;
    checks(i)=check;
-   if check == 0
-        disp(['Invalid frame!']);
+   delim(i,:) = bit(17:24);
+   src(i,:) = bit(25:32);
+   dst(i,:) =bit(33:40);
+   msgtype(i,:) =bit(41:48);
+   msgid(i,:) =bit(49:56);
+   payload(i,:) =bit(57:88);
+   crc(i,:) =bit(89:104);
+   if check == 0 || length(bit)~=113
+        numel = find(data(3,:)~=data(i,:));
+        disp(['Invalid frame: ' num2str(i) ' extra zero at ' num2str(numel(1))]);
    end
 end
+
